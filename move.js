34326cae55126ecb8c2e6beab89e650b7ce95fc6
Alissa <alissamak@outlook.com>
2022-03-23 23:23:24 -0400

    finished assignment and tried out bonus

--- move.js
+++ move.js
@@ -6,7 +6,60 @@ function move(element) {
         element.style.bottom = bottom + 'px'
     }
 
+    function moveWithArrowKeys (left, bottom, callback){
+        let direction = null;
+        let x = left;
+        let y = bottom;
+
+        element.style.left = x + 'px'
+        element.style.bottom = y + 'px'
+
+        function moveCharacter(){
+            if (direction === 'west'){
+                x--;
+            }
+            if(direction === 'north'){
+                y++;
+            }
+            if(direction === 'east'){
+                x++;
+            }
+            if(direction === 'south'){
+                y--;
+            }
+            element.style.left = x + 'px'
+            element.style.bottom = y + 'px'
+            element.style.zIndex = 1;
+        }
+
+        setInterval(moveCharacter, 1)
+
+        document.addEventListener('keydown', function(e){
+            if(e.repeat) return;
+            
+            if (e.key === 'ArrowLeft'){
+                direction = 'west';
+            }
+            if(e.key === 'ArrowUp'){
+                direction ='north';
+            }
+            if(e.key === 'ArrowRight'){
+                direction ='east';
+            }
+            if(e.key === 'ArrowDown'){
+                direction = 'south';
+            }
+            callback(direction);
+        })
+        
+        document.addEventListener('keyup', function(e){
+            direction = null;
+            callback(direction);
+        })
+    }
+
     return {
-        to: moveToCoordinates
+        to: moveToCoordinates,
+        withArrowKeys: moveWithArrowKeys
     }
 }
